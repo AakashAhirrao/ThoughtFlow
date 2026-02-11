d29a39baba2188710ed0715ba125e4363ec81bf3
Aakash Ahirrao <ahirraoaakashni3@gmail.com>
2026-02-11 15:05:54 +0530

    Indentations

--- MyFirstProgram/src/JavaIO/ThoughtLogger.java
+++ MyFirstProgram/src/JavaIO/ThoughtLogger.java
@@ -12,30 +12,30 @@ public class ThoughtLogger {
 
     static String filepath = "";
 
-    public static void fileInitializer(){
+    public static void fileInitializer() {
 
         String timestamp = LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
 
-        filepath =  "record_"+timestamp+".txt";
+        filepath = "record_" + timestamp + ".txt";
     }
 
-    public static String userInput(Scanner sc){
+    public static String userInput(Scanner sc) {
 
         StringBuilder text = new StringBuilder();
-        try{
+        try {
 
             System.out.println("Welcome to Thought Logger!!! 💭💭");
             System.out.println("What's on your mind today (Type 'Q' on a new line to finish)");
             System.out.println("-----------------------------------------------------------");
             System.out.print("--> ");
-            while (true){
+            while (true) {
                 String line = sc.nextLine();
 
-                if(line.equalsIgnoreCase("Q")){
+                if (line.equalsIgnoreCase("Q")) {
                     System.out.print("Exiting Notes.....");
                     System.out.print("\nSaving Notes.");
-                    for (int i=0;i<3;i++){
+                    for (int i = 0; i < 3; i++) {
                         System.out.print(".");
                         Thread.sleep(1000);
                     }
@@ -49,50 +49,44 @@ public class ThoughtLogger {
                 text.append(line).append(System.lineSeparator());
             }
 
-        }
-        catch (InterruptedException e){
+        } catch (InterruptedException e) {
             System.out.println("Program was interrupted when saving");
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Something went wrong while taking input");
         }
         return text.toString();
     }
 
-    public static void FileLogger(String text, String filepath){
-        try(OutputStreamWriter writer = new OutputStreamWriter(
-                new FileOutputStream(filepath), StandardCharsets.UTF_8)){
+    public static void FileLogger(String text, String filepath) {
+        try (OutputStreamWriter writer = new OutputStreamWriter(
+                new FileOutputStream(filepath), StandardCharsets.UTF_8)) {
 
             writer.write(text);
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Something went wrong while logging the thoughts");
         }
     }
 
-    public static void recordReader(String filepath){
-        try(InputStreamReader reader = new InputStreamReader(
-                new FileInputStream(filepath), StandardCharsets.UTF_8)){
+    public static void recordReader(String filepath) {
+        try (InputStreamReader reader = new InputStreamReader(
+                new FileInputStream(filepath), StandardCharsets.UTF_8)) {
             int ch;
-            while((ch = reader.read()) != -1){
+            while ((ch = reader.read()) != -1) {
                 System.out.print((char) ch);
             }
 
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Something went wrong while reading your thoughts :(");
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        try(Scanner sc = new Scanner(System.in)) {
+        try (Scanner sc = new Scanner(System.in)) {
 
             fileInitializer();
             FileLogger(userInput(sc), filepath);
